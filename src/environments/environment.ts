2c8a8e6e43d9666b4b885ff5be7e144aa84cd9fb
Ivan Ulaque <ivanuc19@gmail.com>
2019-10-01 13:48:40 -0400

    Sistema incripciones v1.0

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBrwY9lQ-ma0Mvx__YsDXaU6ZnENiStoao',
+    authDomain: 'inscripcionesescuelas.firebaseapp.com',
+    databaseURL: 'https://inscripcionesescuelas.firebaseio.com',
+    projectId: 'inscripcionesescuelas',
+    storageBucket: '',
+    messagingSenderId: '700453301727',
+    appId: '1:700453301727:web:3577415734867dd2ffa86a'
+
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
